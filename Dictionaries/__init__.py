d769710292a723f7f905e5b90fcc008d4007cf6b
Antony Muga <antonymuga@users.noreply.github.com>
2018-07-18 11:22:28 +0300

    Initial broject bones commit

---
+++ Dictionaries/__init__.py
@@ -0,0 +1,20 @@
+#	++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#	*        RUNTIME SHENG TRANSLATOR - ENGLISH TRACK        *
+#	++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#	*  Welcome to Runtime Sheng translator. v1.0.0           *
+#	*  MIT License, Copyright(c) 2018, Antony Muga           *
+#	*  All Rights Reserved.                                  *
+#	*  Author: Antony Muga                                   *
+#	----------------------------------------------------------
+#	*  Project's Links:                                      *
+#	*  Twitter: https://twitter.com/RuntimeClubKe            *
+#	*  Runtime Club on LinkedIn                              *
+#	*  Runtime Club on Github                                *
+#	*  RuntimeTranslate project on GitHub                    *
+#	----------------------------------------------------------
+#	*  Personal social links:                                *
+#	*  GitHub: https://github.com/antonymuga/                *
+#	*  Website: https://antonymuga.github.io                 *
+#	*  LinkedIn: https://www.linkedin.com/in/antony-muga/    *
+#	*  Email: https://sites.google.com/view/antonymuga/home  *
+#	----------------------------------------------------------
